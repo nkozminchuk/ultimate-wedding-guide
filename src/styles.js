a64241cfdfdb266413fee214c6f80b00df9844f3
nkozminchuk <nadia.kozminchuk@gmail.com>
2026-05-18 10:13:32 -0700

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -47,7 +47,7 @@ export const styles = `
 .cover-bg-overlay {
   position: absolute;
   inset: 0;
-  background: linear-gradient(to bottom, rgba(247,243,236,0.15) 0%, rgba(0,0,0,0.35) 100%);
+  background: linear-gradient(to bottom, rgba(0,0,0,0.45) 0%, rgba(0,0,0,0.65) 100%);
   z-index: 1;
 }
   .cover::before {
